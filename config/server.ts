edb6a1d31a7a7ca647e12158a8efc83cc7819392
Nisha A R <nisha@inspirehealthscience.com>
2025-06-13 17:39:20 +0400

    iniial push for test

---
+++ config/server.ts
@@ -0,0 +1,17 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337), // fallback only
+  app: {
+    keys: env.array('APP_KEYS'),
+  },
+  url: env('URL'),
+  webhooks: {
+    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+  },
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET'),
+    },
+  },
+  allowedHosts: ['NGROK_URL']
+});
